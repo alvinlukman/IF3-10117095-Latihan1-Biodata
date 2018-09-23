aa547890e63b843c6c26906d36f3bcc0f7001d5e
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:12:57 +0700

    Biodata

---
+++ src/if3/pkg10117095/latihan1/biodata/IF310117095Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berfungsi menampilkan Data Diri (Biodata)
+ */
+public class IF310117095Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t\t: Alvin Lukman Nulhakim");
+        System.out.println("ALAMAT\t\t\t: Jln.Dago Pojok");
+        System.out.println("TEMPAT LAHIR\t\t: Subang");
+        System.out.println("TANGGAL LAHIR\t\t: 17 November 1998");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Olahraga & Musik");
+        System.out.println("MAKES\t\t: Jengkol");
+        System.out.println("MIKES\t\t: Es dawet");
+    }
+    
+}
